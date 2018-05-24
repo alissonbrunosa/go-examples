17c035665bfc1f74bac6c3c267eb48cdc6756932
Alisson Bruno <alissonbruno.sa@gmail.com>
2018-05-24 13:22:46 -0300

    gofmt

--- main.go
+++ main.go
@@ -4,17 +4,17 @@ import "stack"
 import "fmt"
 
 type Person struct {
-    Name string
+	Name string
 }
 
 func main() {
-  s := stack.New()
+	s := stack.New()
 
-  fmt.Println(s.IsEmpty())
-  
-  s.Push(&Person{ "James" })
-  s.Push(&Person{ Name: "Doe" })
-  
-  s.Print()
-  fmt.Printf("Stack's length is %d\n", s.Length())
+	fmt.Println(s.IsEmpty())
+
+	s.Push(&Person{"James"})
+	s.Push(&Person{Name: "Doe"})
+
+	s.Print()
+	fmt.Printf("Stack's length is %d\n", s.Length())
 }
--- stack/stack.go
+++ stack/stack.go
@@ -3,54 +3,54 @@ package stack
 import "fmt"
 
 type node struct {
-    Data interface{}
-    Previous *node
+	Data     interface{}
+	Previous *node
 }
 
 type Stack struct {
-    top *node
-    length int
+	top    *node
+	length int
 }
 
 func New() *Stack {
-    return &Stack{}
+	return &Stack{}
 }
 
 func (this *Stack) Push(data interface{}) {
-    if (this.top == nil) {
-        this.top = &node{ Data: data }
-    } else {
-        newTop := &node{ Data: data }
-        newTop.Previous = this.top
-        this.top = newTop
-    }
+	if this.top == nil {
+		this.top = &node{Data: data}
+	} else {
+		newTop := &node{Data: data}
+		newTop.Previous = this.top
+		this.top = newTop
+	}
 
-    this.length += 1
+	this.length += 1
 }
 
 func (this *Stack) Pop() interface{} {
-    top := this.top
-    this.top = top.Previous;
-    this.length -= 1
-    return top.Data
+	top := this.top
+	this.top = top.Previous
+	this.length -= 1
+	return top.Data
 }
 
 func (this *Stack) Top() interface{} {
-    return this.top.Data
+	return this.top.Data
 }
 
 func (this *Stack) Print() {
-    current := this.top
-    for current != nil {
-        fmt.Printf("Value is %v \n", current.Data)
-        current = current.Previous
-    }
+	current := this.top
+	for current != nil {
+		fmt.Printf("Value is %v \n", current.Data)
+		current = current.Previous
+	}
 }
 
 func (this *Stack) IsEmpty() bool {
-    return this.length == 0
+	return this.length == 0
 }
 
 func (this *Stack) Length() int {
-    return this.length
+	return this.length
 }
