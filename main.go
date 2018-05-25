4d2c04c7db7be47663bb5f8197951949d79ba4a6
Alisson Bruno <alissonbruno.sa@gmail.com>
2018-05-25 09:36:16 -0300

    noop main

--- main.go
+++ main.go
@@ -1,20 +1,8 @@
 package main
 
-import "stack"
+import "list"
 import "fmt"
 
-type Person struct {
-	Name string
-}
-
 func main() {
-	s := stack.New()
-
-	fmt.Println(s.IsEmpty())
-
-	s.Push(&Person{"James"})
-	s.Push(&Person{Name: "Doe"})
-
-	s.Print()
-	fmt.Printf("Stack's length is %d\n", s.Length())
+	//noop
 }
